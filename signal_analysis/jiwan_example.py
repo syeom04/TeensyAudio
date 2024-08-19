2cc240390e849fd5abf61371c481f25b5b9dbdb4
syeom04 <yeom.sy334@gmail.com>
2024-08-19 16:29:37 +0900

    working example from jiwan code and used my comp audio recording to create a spectogram of monoaudio

---
+++ signal_analysis/jiwan_example.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+import os
+
+PATH = '/teensy/TeensyAudio/signal_analysis/'
+CURRENT_FILE = 'from_comp.wav'
+sample_rate, samples = wavfile.read(os.path.join(PATH, CURRENT_FILE))
+plt.specgram(samples, Fs=sample_rate)
+plt.show()

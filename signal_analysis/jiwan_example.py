4220cda31d86451eb9d330bbbbe4c1b26696545c
syeom04 <yeom.sy334@gmail.com>
2024-08-20 21:25:38 +0900

    Created own spectrogram python script to avg stereo into mono

--- signal_analysis/jiwan_example.py
+++ signal_analysis/jiwan_example.py
@@ -4,7 +4,7 @@ from scipy.io import wavfile
 import os
 
 PATH = '/teensy/TeensyAudio/signal_analysis/'
-CURRENT_FILE = 'from_comp.wav'
+CURRENT_FILE = 'RECORD.wav'
 sample_rate, samples = wavfile.read(os.path.join(PATH, CURRENT_FILE))
 plt.specgram(samples, Fs=sample_rate)
 plt.show()
---
+++ signal_analysis/plot_spectrogram.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import os
+import numpy as np
+
+PATH = '/teensy/TeensyAudio/signal_analysis/'
+CURRENT_FILE = 'RECORD.wav'
+sample_rate, samples = wavfile.read(os.path.join(PATH, CURRENT_FILE))
+
+# If the audio is stereo (2 channels), convert it to mono by averaging the two channels
+if len(samples.shape) == 2:
+    samples = np.mean(samples, axis=1)
+
+plt.figure(figsize=(10, 6))
+plt.specgram(samples, Fs=sample_rate, NFFT=1024, noverlap=512, cmap='viridis')
+plt.title('Spectrogram of {}'.format(CURRENT_FILE))
+plt.xlabel('Time [s]')
+plt.ylabel('Frequency [Hz]')
+plt.colorbar(label='Intensity [dB]')
+plt.show()
